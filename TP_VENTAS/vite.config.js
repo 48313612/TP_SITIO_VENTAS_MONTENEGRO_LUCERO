021f6d195063fbb8a0f3e3ab9b8786806ba66e39
47872151 <47872151@est.ort.edu.ar>
2025-06-28 21:51:26 -0300

    final

--- TP_VENTAS/vite.config.js
+++ TP_VENTAS/vite.config.js
@@ -5,5 +5,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     historyApiFallback: true
-  }
+  },
+  base: 'https://github.com/48313612/TP_SITIO_VENTAS_MONTENEGRO_LUCERO/'
 })
