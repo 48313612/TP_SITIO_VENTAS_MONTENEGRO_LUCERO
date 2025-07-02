cfefe0ef2117cb131ed2dc3ecac1dc3796a20eb9
PALOMA MONTENEGRO <47872151@ort>
2025-07-02 07:55:02 -0300

    listo

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/TP_SITIO_VENTAS_MONTENEGRO_LUCERO/',
+  base: '/',
   plugins: [react()],
 })
